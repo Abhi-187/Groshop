696bca73e69b991da172602f9ce412ecdb0239a2
influx <71868156+Abhi-187@users.noreply.github.com>
2024-01-29 14:47:55 +0530

    Add files via upload

---
+++ TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  login
+//
+//  Created by Zeal Shah  on 19/02/23.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
